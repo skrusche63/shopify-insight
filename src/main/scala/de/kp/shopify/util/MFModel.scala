3fb53da0d5e9145461be74f5447da5d3efd13c45
skrusche63 <skrusche63@googlemail.com>
2015-01-21 08:32:50 +0100

    CosineSimilarity added.

--- src/main/scala/de/kp/shopify/util/MFModel.scala
+++ src/main/scala/de/kp/shopify/util/MFModel.scala
@@ -24,6 +24,8 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
 
 import org.apache.spark.mllib.recommendation.Rating
+import org.apache.spark.mllib.linalg.Vectors
+
 /**
  * MFModel is a copy of Apache Spark's MatrixFactorizationModel that is still [private] 
  * for the version used here. As we need to persist the results of collaborative filtering 
@@ -33,8 +35,69 @@ class MFModel (
     val rank:Int,
     val rmse:Double,
     val userFeatures:RDD[(Int,Array[Double])],
-    val productFeatures:RDD[(Int,Array[Double])]) extends Serializable {
+    val productFeatures:RDD[(Int,Array[Double])]
+  ) extends Serializable {
 
+  /**
+   * This method is added to the MFModel and differs from Apache Spark's 
+   * implementation of the MatrixFactorizationModel; it has been introduced
+   * to support item-item similarity based on their latent feature vectors
+   * 
+   * THIS METHOD DOES NOT WORK IN A DISTRIBUTED MANNER
+   * 
+   */
+  def computeProductSimilarities:DoubleMatrix = {
+    
+    val dataset = productFeatures.sortBy(_._1).map(_._2).collect
+    
+    val dim = dataset.size
+    val matrix = DoubleMatrix.zeros(dim,dim)
+    /*
+     * Compute the cosine similarity of the product features 
+     * vectors (rows) and ignore diagonal values (=0), due to
+     * the fact, that this matrix will be used to determine
+     * most similar other products
+     */
+    (0 until dim).foreach(i => {
+      (0 until dim).foreach(j => {
+        if (i != j) matrix.put(i,j,CosineSimilarity.compute(dataset(i),dataset(j)))
+      })
+    })
+    
+    matrix
+    
+  }
+   
+  /**
+   * This method is added to the MFModel and differs from Apache Spark's 
+   * implementation of the MatrixFactorizationModel; it has been introduced
+   * to support user-user similarity based on their latent feature vectors
+   * 
+   * THIS METHOD DOES NOT WORK IN A DISTRIBUTED MANNER
+   * 
+   */
+  def computeUserSimilarities:DoubleMatrix = {
+     
+    val dataset = userFeatures.sortBy(_._1).map(_._2).collect
+    
+    val dim = dataset.size
+    val matrix = DoubleMatrix.zeros(dim,dim)
+    /*
+     * Compute the cosine similarity of the user features 
+     * vectors (rows) and ignore diagonal values (=0), due to
+     * the fact, that this matrix will be used to determine
+     * most similar other users
+     */
+    (0 until dim).foreach(i => {
+      (0 until dim).foreach(j => {
+        if (i != j) matrix.put(i,j,CosineSimilarity.compute(dataset(i),dataset(j)))
+      })
+    })
+    
+    matrix
+    
+  }
+ 
   def predict(user: Int, product: Int): Double = {
 
     val userVector = new DoubleMatrix(userFeatures.lookup(user).head)
@@ -44,38 +107,34 @@ class MFModel (
   
   }
 
-  def predict(usersProducts: RDD[(Int, Int)]): RDD[Rating] = {
+  def predict(usersProducts:RDD[(Int,Int)]):RDD[Rating] = {
    
-    val users = userFeatures.join(usersProducts).map{
-      case (user, (uFeatures, product)) => (product, (user, uFeatures))
-    }
+    val users = userFeatures.join(usersProducts).map{case (user,(uFeatures,product)) => (product,(user,uFeatures))}    
+    users.join(productFeatures).map{case (product, ((user, uFeatures), pFeatures)) => {
     
-    users.join(productFeatures).map {
-      case (product, ((user, uFeatures), pFeatures)) =>
-        val userVector = new DoubleMatrix(uFeatures)
-        val productVector = new DoubleMatrix(pFeatures)
-        Rating(user, product, userVector.dot(productVector))
-    }
+      val userVector = new DoubleMatrix(uFeatures)
+      val productVector = new DoubleMatrix(pFeatures)
+      
+      Rating(user, product, userVector.dot(productVector))
+    
+    }}
   
   }
 
-  def recommendProducts(user: Int, num: Int): Array[Rating] =
-    recommend(userFeatures.lookup(user).head, productFeatures, num)
-      .map(t => Rating(user, t._1, t._2))
+  def recommendProducts(user:Int,num:Int):Array[Rating] =
+    recommend(userFeatures.lookup(user).head, productFeatures, num).map(t => Rating(user, t._1, t._2))
 
   def recommendUsers(product: Int, num: Int): Array[Rating] =
-    recommend(productFeatures.lookup(product).head, userFeatures, num)
-      .map(t => Rating(t._1, product, t._2))
+    recommend(productFeatures.lookup(product).head, userFeatures, num).map(t => Rating(t._1, product, t._2))
 
-  private def recommend(
-      recommendToFeatures: Array[Double],
-      recommendableFeatures: RDD[(Int, Array[Double])],
-      num: Int): Array[(Int, Double)] = {
+  private def recommend(recommendToFeatures: Array[Double],recommendableFeatures: RDD[(Int, Array[Double])],num: Int): Array[(Int, Double)] = {
+    
     val recommendToVector = new DoubleMatrix(recommendToFeatures)
     val scored = recommendableFeatures.map { case (id,features) =>
       (id, recommendToVector.dot(new DoubleMatrix(features)))
     }
+    
     scored.top(num)(Ordering.by(_._2))
-  }
-
+  
+  }  
 }
